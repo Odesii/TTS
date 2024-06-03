e9c0bf64cbbb3d748b80d11f9f28f25764a52340
Katriel Chiu <katriel_chiu@msn.com>
2024-06-03 13:38:10 -0700

    set up main routes to render the correct page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+// import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
+import Home from './components/Home/index.jsx';
+import Login from './components/Login/index.jsx';
+import Register from './components/Register/index.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <h1>Wrong page!</h1>,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      }, {
+        path: '/login',
+        element: <Login />
+      }, {
+        path: '/register',
+        element: <Register />
+      }
+    ]
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )

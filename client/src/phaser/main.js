7893fbd39830dee286973a83cd3f35b0f20bcda7
Odesii <alic.madd@gmail.com>
2024-06-03 14:33:48 -0400

    file setup for Phaser with react

---
+++ client/src/phaser/main.js
@@ -0,0 +1,30 @@
+import Phaser from 'phaser';
+import { WorldScene } from './scenes/worldScene.js';
+
+const config = {
+    type: Phaser.AUTO,
+    width: 240,
+    height: 160,
+    parent: 'game-container', // Moved out of the scale object
+    scene: [WorldScene],
+    scale: {
+        mode: Phaser.Scale.FILL,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true,
+            // debug: false
+        }
+    },
+    pixelArt: true,
+    canvasStyle: `display: block; width: 100%; height: 100%;`,
+    autoFocus: true,
+};
+
+const StartGame = (parent) => { 
+    return new Phaser.Game({ ...config, parent });
+};
+
+export default StartGame

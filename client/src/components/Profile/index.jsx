0b33e021ef102cbe813499c2b52ca68389fe28e4
Katriel Chiu <katriel_chiu@msn.com>
2024-06-05 10:20:35 -0700

    added a user profile page

---
+++ client/src/components/Profile/index.jsx
@@ -0,0 +1,34 @@
+import { useQuery } from '@apollo/client';
+import { GET_USER } from '../../utils/queries';
+import Auth from '../../utils/auth';
+import './style.css';
+
+function Profile() {
+    const { loading, data } = useQuery(GET_USER);
+    const userData = data?.myProfile || {};
+
+    if (loading) {
+        return <h2>LOADING...</h2>
+    }
+
+    function redirect() {
+        window.location.replace('/login');
+    }
+
+    return (
+        <section className="layout">
+            {Auth.loggedIn() ? (
+                <>
+                    <h2>Welcome back, {userData.username}</h2>
+                </>
+            ) : (
+                <>
+                    <h2>You must be logged in to view this page.</h2>
+                    {redirect()}
+                </>
+            )}
+        </section>
+    )
+}
+
+export default Profile;

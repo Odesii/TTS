da02e416b0f55c4b4c2dd3ffe23a74baa62c5377
Katriel Chiu <katriel_chiu@msn.com>
2024-06-05 11:55:01 -0700

    added navigation bar and page components that renders on click

--- client/src/components/Profile/index.jsx
+++ client/src/components/Profile/index.jsx
@@ -1,11 +1,25 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { GET_USER } from '../../utils/queries';
 import Auth from '../../utils/auth';
+import Nav from '../Nav';
+import Page from '../Page';
 import './style.css';
 
 function Profile() {
     const { loading, data } = useQuery(GET_USER);
     const userData = data?.myProfile || {};
+
+    const [pages] = useState([
+        { page: "Profile" },
+        { page: "Email" },
+        { page: "Password" },
+        { page: "Delete" }
+    ]);
+    
+    // Use this to change the state of the pages
+    const [current, setCurrent] = useState(pages[0]);
 
     if (loading) {
         return <h2>LOADING...</h2>
@@ -19,8 +33,14 @@ function Profile() {
         <section className="layout">
             {Auth.loggedIn() ? (
                 <>
+                    <Link to='/'>
+                        Go home
+                    </Link>
+                    <Nav pages={pages} current={current} setCurrent={setCurrent} />
                     <h2>Welcome to your profile, {userData.username}</h2>
-                    <h3>Shrooms: <span className='shrooms'></span> </h3>
+                    <h3>Shrooms: <span className="shrooms"></span> </h3>
+                    
+                    <Page current={current} />
                 </>
             ) : (
                 <>

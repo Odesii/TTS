f7f9f53cc0a8db72cad6a5dd30623985bb41b18d
Katriel Chiu <katriel_chiu@msn.com>
2024-06-07 13:37:47 -0700

    removed testing data function

--- client/src/components/Profile/ProfileMain/index.jsx
+++ client/src/components/Profile/ProfileMain/index.jsx
@@ -1,5 +1,5 @@
 import { useQuery } from '@apollo/client';
-import { GET_USER, GET_ITEMS } from '../../../utils/queries';
+import { GET_USER } from '../../../utils/queries';
 import './style.css';
 
 function ProfileMain() {
@@ -10,36 +10,10 @@ function ProfileMain() {
         return <h2>LOADING...</h2>
     }
 
-    function test() {
-        const items = getItems();
-        for (let i = 0; i < items.length; i++) {
-            let count = 0;
-
-            for (let j = 0; j < userData.inventory.length; j++) {
-                if (items[i]._id === userData.inventory[j]._id) {
-                    count = count + 1;
-                }
-            }
-            console.log(count);
-        }
-        // console.log(userData.inventory);
-        // console.log(userData.healthPotions);
-        // console.log(userData.get('attackPotions'));
-        // console.log(userData.get('defensePotions'));
-        // console.log(userData);
-    }
-
-    function getItems() {
-        const { loading, data } = useQuery(GET_ITEMS);
-        const itemData = data?.stockShop || {};
-        return itemData;
-    }
-
     return (
         <>
             <h2>Welcome to your profile, {userData.username}</h2>
             <h3>Shrooms: <span className="shrooms"></span> </h3>
-            {test()}
         </>
     )
 }

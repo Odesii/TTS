80444f17867113e049d71f5397678de057e580e2
Odesii <alic.madd@gmail.com>
2024-06-08 15:37:56 -0400

    weq

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = import.meta.env.VITE_DEPLOYED_URL || 'http://localhost:3001';
+const SOCKET_URL = import.meta.env.VITE_DEPLOYED_URL;
 const socket = io(SOCKET_URL);
 
 export default socket;
